bfaacca39027d47276578b579d9eb9695ece5668
Ashutosh Prasad <45146282+ASHUTOSH57211@users.noreply.github.com>
2021-10-01 00:06:48 +0530

    Update printArray.java

--- printArray.java
+++ printArray.java
@@ -15,6 +15,22 @@ public static void main(String args[]){
   int[] arr3 = new int[n2];
   for(int val : arr3){
     arr3[val] = sc.nextInt();
+    
+        String str = sc.nextLine();
+        int len = str.length();
+        int max = 0;
+        for(int i = 0; i<len ; i++){
+            int temp = 0;
+            for(int j = 0;j< len;j++){
+                if(str[i]==str[j]){
+                    temp=temp++;
+                }
+            }
+            if(temp > max){
+                max=temp;
+            }
+        }
+        System.out.println(max+"The hisgest number of letters in the word"+str);
   }
 }
   
